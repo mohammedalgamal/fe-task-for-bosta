78815a95326e277ded6a13f621c0656ff80bfc1d
mohammedalgamal <mohammedalgamal95@gmail.com>
2024-10-16 19:52:24 +0300

    fix: remove eslint config comments

--- eslint.config.js
+++ eslint.config.js
@@ -8,12 +8,9 @@ export default tseslint.config(
   {
     settings: { react: { version: "18.3" } },
     plugins: {
-      // Add the react plugin
       react,
     },
     rules: {
-      // other rules...
-      // Enable its recommended rules
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
     },
