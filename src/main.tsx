21f41b9ceb027037b2f5a994d73a610ac157dcd6
mohammedalgamal <mohammedalgamal95@gmail.com>
2024-10-18 00:19:40 +0300

    fix: rename contexts folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { LocaleProvider } from "./Contexts/useLocaleContext/useLocaleContext.tsx";
-import { DataProvider } from "./Contexts/useDataContext/useDataContext.tsx";
+import { LocaleProvider } from "./contexts/useLocaleContext/useLocaleContext.tsx";
+import { DataProvider } from "./contexts/useDataContext/useDataContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
